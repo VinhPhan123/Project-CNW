dc8ffd74891ae458f877cf45a9fb4883dbce5527
nguyenanhtai <anhtai22042004@gmail.com>
2024-09-05 15:11:53 +0700

    add files

--- dangky.php
+++ dangky.php
@@ -1,5 +1,11 @@
 <?php 
     include './laytouts/header.php';
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+	
+	require 'vendor/PHPMailer/src/Exception.php';
+	require 'vendor/PHPMailer/src/PHPMailer.php';
+	require 'vendor/PHPMailer/src/SMTP.php';
 ?>
 
 <?php 
@@ -9,7 +15,7 @@
 	// echo $_POST['_token'] . '<br>'; // biến $_POST['_token'] không thay đổi vì nó không thực hiện submit
 	// echo $_SESSION['token'];
 
-    if(isset($_POST['submit']) && $_SESSION['token'] == $_POST['_token']) {
+    if(isset($_REQUEST['submit']) && $_SESSION['token'] == $_POST['_token']) {
 
         $hoVaTen = $_POST['hoVaTen'];
         $taiKhoan = $_POST['taiKhoan'];
@@ -21,30 +27,32 @@
         $soDienThoai = $_POST['soDienThoai'];
         $email = $_POST['email'];
 
+		echo "1231231322";
+
 		
-		$s = "SELECT taiKhoan FROM user WHERE taiKhoan='$taiKhoan'";
-		$query = mysqli_query($connect, $s);
-		if(mysqli_num_rows($query) > 0){
-			$error_taiKhoan = 'Tài khoản đã tồn tại';
-		} else {
-			$sql = "INSERT INTO user(hoVaTen, taiKhoan, matKhau, gioiTinh, ngaySinh, diaChi, soDienThoai, email) VALUES 
-			('$hoVaTen', '$taiKhoan', '$matKhau_hashed', '$gioiTinh', '$ngaySinh', '$diaChi', '$soDienThoai', '$email');";
+		// $s = "SELECT taiKhoan FROM user WHERE taiKhoan='$taiKhoan'";
+		// $query = mysqli_query($connect, $s);
+		// if(mysqli_num_rows($query) > 0){
+		// 	$error_taiKhoan = 'Tài khoản đã tồn tại';
+		// } else {
+		// 	$sql = "INSERT INTO user(hoVaTen, taiKhoan, matKhau, gioiTinh, ngaySinh, diaChi, soDienThoai, email) VALUES 
+		// 	('$hoVaTen', '$taiKhoan', '$matKhau_hashed', '$gioiTinh', '$ngaySinh', '$diaChi', '$soDienThoai', '$email');";
 	
-		   $result = mysqli_query($connect, $sql);
+		//    $result = mysqli_query($connect, $sql);
 	
-			if($result){
-				$affected_row = mysqli_affected_rows($connect);
-				if($affected_row > 0){
-					$_SESSION['taiKhoan'] = $taiKhoan;
-					?>
-					<script>
-						alert("Xin chào <?php $hoVaTen?>");
-						window.location.href="index.php";
-					</script>
-				<?php 
-				}
-			}
-		}
+		// 	if($result){
+		// 		$affected_row = mysqli_affected_rows($connect);
+		// 		if($affected_row > 0){
+		// 			$_SESSION['taiKhoan'] = $taiKhoan;
+		// 			?>
+		// 			<script>
+		// 				alert("Xin chào <?php $hoVaTen?>");
+		// 				window.location.href="index.php";
+		// 			</script>
+		// 		<?php 
+		// 		}
+		// 	}
+		// }
     }
 ?>
 
@@ -115,6 +123,22 @@
 					    <label for="dongYNhanMail" class="form-label">Đồng ý nhận email</label>
 					    <input type="checkbox" class="form-check-input" id="dongYNhanMail" name="dongYNhanMail">
 				  	</div>
+
+					<!-- Teacher form -->
+					<div class="mb-3">
+						<label for="dangKyGiaoVien" class="form-label">Đăng ký với tư cách giáo viên</label>
+						<input type="checkbox" class="form-check-input" id="dangKyGiaoVien" name="dangKyGiaoVien" required="required" onchange="xuLyDangKyGiaoVien()">
+					</div>
+
+					<div class="mb-3" style="visibility: hidden;" id="dangKy">
+						<label for="">Nhận mã gửi tới gmail</label>
+						<div class="btn btn-primary" style="margin-left: 20px;" name="btn_send" id="btn_send" onclick="nhanMa()">Send</div>
+						<div style="height: 20px;"></div>
+						<label for="">Nhập passcode được cung cấp với tư cách giáo viên</label>
+						<input type="text" class="form-control" id="passcode" >
+					</div>
+
+
 			  	</div>
 			  	<input type="submit" class="btn btn-primary" value="Đăng ký" name="submit" id="submit" style="visibility: hidden;"/>
 
@@ -146,9 +170,55 @@
 			document.getElementById("submit").style.visibility = "hidden";
 		}
 	}
-	
-</script>
 
+	function xuLyDangKyGiaoVien(){
+		dangKyGiaoVien = document.getElementById("dangKyGiaoVien");
+		if(dangKyGiaoVien.checked==true){
+			document.getElementById("dangKy").style.visibility="visible";
+		} else {
+			document.getElementById("dangKy").style.visibility = "hidden";
+		}
+	}
+
+	function nhanMa(){
+		// <?php 
+			
+		// 	//Create an instance; passing `true` enables exceptions
+		// 	// $mail = new PHPMailer(true);
+			
+		// 	$s = "SELECT email FROM admins;";
+		// 	$email_admin = mysqli_query($connect, $s);
+
+		// 	echo $email_admin;
+
+
+		// 	// try {
+		// 	// 	//Server settings
+		// 	// 	$mail->isSMTP();                                            //Send using SMTP
+		// 	// 	$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+		// 	// 	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+		// 	// 	$mail->Username   = 'anhtai22042004@gmail.com';                     //SMTP username
+		// 	// 	$mail->Password   = 'smab floy tdrw zizh';                               //SMTP password
+		// 	// 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+		// 	// 	$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+			
+		// 	// 	//Recipients
+		// 	// 	$mail->setFrom($email, 'Mailer');
+		// 	// 	$mail->addAddress($email, 'Anh Tai');     //Add a recipient
+			
+		// 	// 	//Content
+		// 	// 	$mail->isHTML(true);                                  //Set email format to HTML
+		// 	// 	$mail->Subject = 'Test main';
+		// 	// 	$mail->Body    = '<b>New content</b>';
+			
+		// 	// 	$mail->send();
+		// 	// 	echo 'Message has been sent';
+		// 	// } catch (Exception $e) {
+		// 	// 	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+		// 	// }			
+		// ?>
+	}
+</script>
 
 
 <?php 
